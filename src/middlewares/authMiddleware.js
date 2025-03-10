837e80944cc0757aee514b41ba8f9a9ec127eaf8
ayush-singh-mechademy <135104422+ayush-singh-mechademy@users.noreply.github.com>
2025-03-11 00:40:51 +0530

    add : auth middleware to protect api route

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const SUPABASE_JWT_SECRET = process.env.SUPABASE_JWT_SECRET;
+
+const authenticateUser = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+
+    const decoded = jwt.verify(token, SUPABASE_JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+module.exports = authenticateUser;
